7bc5451fe8dff7ec634856e3de3a43931e7731d5
David Nunez <david@davidnunez.com>
2013-03-21 12:14:58 -0400

    Phoneme Prefab initial commit

--- Assets/_scripts/Director.cs
+++ Assets/_scripts/Director.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class Director : MonoBehaviour {
 	
 	public int numberOfLetters = 10;
-	
+	public Letter[] letters;
+	public Phoneme phoneme;
 	// Use this for initialization
-	void OnEnable () {
-	
+	void Awake () {
+		letters = new Letter[numberOfLetters];
 		for (int i = 0; i < numberOfLetters; i++) {
 			GameObject go = (GameObject)Instantiate (Resources.Load ("_prefabs/LetterPrefab"));	
 			Letter letter = go.GetComponent<Letter>();
@@ -19,11 +20,26 @@ public class Director : MonoBehaviour {
 			go.renderer.material.mainTexture = tex;
 			
 			Debug.Log (textureName);
+			letters[i] = letter;
 		}
+
+		GameObject phonemeObject = (GameObject)Instantiate (Resources.Load ("_prefabs/PhonemePrefab"));
+		phoneme = phonemeObject.GetComponent<Phoneme>();
+	}
+	void Start() {
+		setupPhoneme();
+		phoneme.PlayClip();
+	}
+
+	void setupPhoneme(){
+		phoneme.setLetter(letters[Random.Range(0, letters.Length)].letter);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
+	
+	
+	
 }
--- Assets/_scripts/Phoneme.cs
+++ Assets/_scripts/Phoneme.cs
@@ -5,9 +5,13 @@ public class Phoneme : MonoBehaviour {
 	public static string[] VOICES = new string[] {"filip_dirty_test"}; // TODO: this could probably go elsewhere 
 	public string letter;
 	public string voice;
+	public AudioSource audioSource;
+	public AudioClip audioClip;
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		voice = VOICES[0];
+		audioSource = Camera.mainCamera.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -15,7 +19,17 @@ public class Phoneme : MonoBehaviour {
 	
 	}
 	
-	void filename() {
+	string filename() {
 		return letter;
 	}
+	
+	public void setLetter(string l) {
+		letter = l;
+		audioClip = Resources.Load(voice + "/" + filename()) as AudioClip;
+		Debug.Log ("Setting phoneme mp3 to:" + voice +"/" +filename());
+	}
+	public void PlayClip() {
+		audioSource.PlayOneShot(audioClip);
+	}
+
 }
