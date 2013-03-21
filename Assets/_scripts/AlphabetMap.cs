b17c202dac4cb58a7c7bcb1c7ef16980917a90d9
David Nunez <david@davidnunez.com>
2013-03-21 10:09:23 -0400

    Initial implementation of letter class

---
+++ Assets/_scripts/AlphabetMap.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.Collections;
+
+public class AlphabetMap : MonoBehaviour {
+	
+	public static Hashtable letterToIndex = new Hashtable();
+	public static Hashtable indexToLetter = new Hashtable();
+
+	// Use this for initialization
+	void Start () {
+		letterToIndex.Add("a", 1);
+		letterToIndex.Add("b", 2);
+		letterToIndex.Add("c", 3);
+		letterToIndex.Add("d", 4);
+		letterToIndex.Add("e", 5);
+		letterToIndex.Add("f", 6);
+		letterToIndex.Add("g", 7);
+		letterToIndex.Add("h", 8);
+		letterToIndex.Add("i", 9);
+		letterToIndex.Add("j", 10);
+		letterToIndex.Add("k", 11);
+		letterToIndex.Add("l", 12);
+		letterToIndex.Add("m", 13);
+		letterToIndex.Add("n", 14);
+		letterToIndex.Add("o", 15);
+		letterToIndex.Add("p", 16);
+		letterToIndex.Add("q", 17);
+		letterToIndex.Add("r", 18);
+		letterToIndex.Add("s", 19);
+		letterToIndex.Add("t", 20);
+		letterToIndex.Add("u", 21);
+		letterToIndex.Add("v", 22);
+		letterToIndex.Add("w", 23);
+		letterToIndex.Add("x", 24);
+		letterToIndex.Add("y", 25);
+		letterToIndex.Add("z", 26);
+		
+		indexToLetter.Add (1, "a");
+		indexToLetter.Add (2, "b");
+		indexToLetter.Add (3, "c");
+		indexToLetter.Add (4, "d");
+		indexToLetter.Add (5, "e");
+		indexToLetter.Add (6, "f");
+		indexToLetter.Add (7, "g");
+		indexToLetter.Add (8, "h");
+		indexToLetter.Add (9, "i");
+		indexToLetter.Add (10, "j");
+		indexToLetter.Add (11, "k");
+		indexToLetter.Add (12, "l");
+		indexToLetter.Add (13, "m");
+		indexToLetter.Add (14, "n");
+		indexToLetter.Add (15, "o");
+		indexToLetter.Add (16, "p");
+		indexToLetter.Add (17, "q");
+		indexToLetter.Add (18, "r");
+		indexToLetter.Add (19, "s");
+		indexToLetter.Add (20, "t");
+		indexToLetter.Add (21, "u");
+		indexToLetter.Add (22, "v");
+		indexToLetter.Add (23, "w");
+		indexToLetter.Add (24, "x");
+		indexToLetter.Add (25, "y");
+		indexToLetter.Add (26, "z");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	static public string getLetter(int index) {
+		return (string)letterToIndex[index];
+	}
+
+	static public int getIndex(string letter) {
+		return (int) letterToIndex[letter];
+	}
+
+	
+}
---
+++ Assets/_scripts/Letter.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+
+/*
+ * class letter {
+--every letter we display is it’s own letter object associated with a specific filename and specific attributes. Thus, we will easily be able to track what the kids tapped. 
+--fields
+filename: string. unique ID as well as the reference path. all letters are stored as white fill on transparent background in  png (masters in ai illustrator files).
+letter: string. what letter it is (a-z).
+font: string. what font it belongs to. (Helvetica high bold)
+typeface: string. what typeface it is. (Helvetica) maybe inherited from font.
+uppercase: boolean. maybe inherited from font.
+italic: boolean. maybe inherited from font.
+bold:boolean. maybe inherited from font.
+serif:boolean. maybe inherited from font.
+letterSets: array. info to which letterSets the letter belongs to. 
+}
+ */
+
+public class Letter : MonoBehaviour {
+	public static string[] TYPEFACES = new string[] {"courierNew", "museo", "philosopher"}; // TODO: this could probably go elsewhere 
+	public string letter;
+	public string typeface;
+	public bool uppercase;
+	
+	// TODO: decide if these are needed:
+	// public string font; // maybe we don't need this?
+	//public bool italic;
+	//public bool bold;
+	//public bool serif;
+	
+	
+	// LetterSet[] letterSets; // TODO: Implement Letter Set
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}	
+	
+	void Randomize() {// will need to make this function more robust / customizable
+		letter = AlphabetMap.getLetter(Random.Range(0,26));
+		typeface = TYPEFACES[Random.Range(0, TYPEFACES.Length)];
+		
+		
+		uppercase = Random.Range(0,2) == 1;
+
+	}
+
+	
+	
+	public string filename() {
+		string highlow;
+		if (uppercase) {
+			highlow = "High";
+		} else {
+			highlow = "Low";
+		}
+		return typeface + highlow + "-" + AlphabetMap.getIndex(letter) + ".jpg";
+	}
+}
